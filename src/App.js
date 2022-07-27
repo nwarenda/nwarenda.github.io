06cf78bedfbac3b907617da315400c283d59b350
nickwar42 <nwarenda@accutroninstruments.com>
2022-07-27 14:13:13 -0400

    Added a bunch of new React files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { React, Card } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HighNoon from './components/HighNoon';
+import Navbar from './components/Navbar';
 //import Layout from './components/Layout';
 
-const App = () => {
+function App () {
 
-	document.title = "";
+	{/*	<BrowserRouter basename="/">
+		{/*	<HighNoon/>
+      {/* <Layout/>
+		</BrowserRouter> */}
 
 	return (
-		<BrowserRouter basename="/">
-			<HighNoon/>
-      {/* <Layout/> */}
-		</BrowserRouter>
+		<>
+		<Router>
+				<Navbar />
+				<Routes> 
+					<Route path='/' exact />
+				</Routes>
+				</Router>
+		</>
 	);
-};
+}
 
 export default App;
--- src/components/HighNoon.js
+++ src/components/HighNoon.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import '../App.css';
 
-class HighNoon extends React.Component {
+class HighNoon {
     render(){
         return (
             <div>
